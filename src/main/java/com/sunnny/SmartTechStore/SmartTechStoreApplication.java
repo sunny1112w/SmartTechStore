2821514baeb422e28f66f6ffee977f7df6aad31c
sunny1112w <duyn2280z@gmail.com>
2024-10-25 17:31:56 +0700

    Demo shop

---
+++ src/main/java/com/sunnny/SmartTechStore/SmartTechStoreApplication.java
@@ -0,0 +1,13 @@
+package com.sunnny.SmartTechStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTechStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTechStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunnny/SmartTechStore/SmartTechStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunnny.SmartTechStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTechStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
